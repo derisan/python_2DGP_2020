ae4b2bf9026df0d7ef6ad6976890bdba2ae9d415
derisan <derisan@gmail.com>
2020-10-25 21:58:26 +0900

    hp, mp 그리기. 그리기 방식 변경(stretch)

--- 기말프로젝트/mushrun/life_gauge.py
+++ 기말프로젝트/mushrun/life_gauge.py
@@ -5,35 +5,74 @@ import gobj
 class LifeGauge:
     MAX_HP = 10
     MAX_MP = 10
+    BG, HP, MP, EXP = range(4)
 
     def __init__(self, x, y):
         self.pos = x, y
 
-        # self.hp = LifeGauge.MAX_HP
-        self.hp = 8
+        self.hp = LifeGauge.MAX_HP
         self.mp = LifeGauge.MAX_MP
         self.exp = 0
 
-        self.bg = gfw.image.load(gobj.res('tags/bar3.png'))
-        self.hp_0 = gfw.image.load(gobj.res('tags/hp/hp0.png'))
-        self.hp_1 = gfw.image.load(gobj.res('tags/hp/hp1.png'))
-        self.hp_2 = gfw.image.load(gobj.res('tags/hp/hp2.png'))
+        self.images = None
+        self.get_images()
+
+    def get_images(self):
+        """
+        Load images used in class
+        """
+        self.images = [[] for _ in range(3 + 1)]
+
+        self.images[LifeGauge.BG].append(gfw.image.load(gobj.res('tags/bar3.png')))
+
+        for i in range(2 + 1):
+            self.images[LifeGauge.HP].append(gfw.image.load(gobj.res('tags/hp/hp' + str(i) + '.png')))
+
+        for i in range(2 + 1):
+            self.images[LifeGauge.MP].append(gfw.image.load(gobj.res('tags/mp/mp' + str(i) + '.png')))
+
+        for i in range(1 + 1):
+            self.images[LifeGauge.EXP].append(gfw.image.load(gobj.res('tags/exp/exp' + str(i) + '.png')))
 
     def draw(self):
-        self.bg.draw_to_origin(*self.pos)
-        x = self.pos[0] + 9
-        y = self.pos[1] + 3
-        self.hp_0.draw_to_origin(x, y)
+        self.images[LifeGauge.BG][0].draw_to_origin(*self.pos)
 
-        for i in range(1, self.hp):
-            x = self.pos[0] + 11 * i
-            y = self.pos[1] + 3
-            self.hp_1.draw_to_origin(x, y)
+        self.hp_draw()
+        self.mp_draw()
+        self.exp_draw()
 
-        if self.hp == LifeGauge.MAX_HP:
-            x = self.pos[0] + 11 * self.hp
+    def hp_draw(self):
+        if self.hp > 0:
+            x = self.pos[0] + 9
             y = self.pos[1] + 3
-            self.hp_2.draw_to_origin(x, y)
+            self.images[LifeGauge.HP][0].draw_to_origin(x, y)
+
+            x += 2
+            h = self.images[LifeGauge.HP][1].h
+            self.images[LifeGauge.HP][1].draw_to_origin(x, y, w=11 * (self.hp - 1), h=h)
+
+            if self.hp == LifeGauge.MAX_HP:
+                x = self.pos[0] + 11 * self.hp
+                y = self.pos[1] + 3
+                self.images[LifeGauge.HP][2].draw_to_origin(x, y)
+
+    def mp_draw(self):
+        if self.mp > 0:
+            x = self.pos[0] + 115
+            y = self.pos[1] + 3
+            self.images[LifeGauge.MP][0].draw_to_origin(x, y)
+
+            x += 2
+            h = self.images[LifeGauge.MP][1].h
+            self.images[LifeGauge.MP][1].draw_to_origin(x, y, w=11 * (self.mp - 1), h=h)
+
+            if self.mp == LifeGauge.MAX_HP:
+                x += 11 * (self.mp - 1)
+                y = self.pos[1] + 3
+                self.images[LifeGauge.MP][2].draw_to_origin(x, y)
+
+    def exp_draw(self):
+        pass
 
     def update(self):
         pass
